fd24b66a7139004b8dce2d9bda96b413b6ac1279
liliya komarova <liliya.komarova.86@mail.ru>
2023-01-05 16:01:35 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.example.Person;
+import org.example.PersonBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person mom = new PersonBuilder()
+                .setName("Лилия")
+                .setName("Ольга")
+                .setSurname("Георг")
+                .setSurname("Комаревцева")
+                .setAge(31)
+                .setAddress("Москва")
+                .build();
+        Person son = mom.newChildBuilder()
+                .setName("Алексей")
+                .build();
+        mom.happyBirthday();
+
+        System.out.println("У " + mom + " есть сын, " + son);
+
+        try {
+            Person mom1 = new PersonBuilder()
+                    .setName("Ольга")
+                    .setAge(31)
+                    .setAddress("Москва")
+                    .build();
+            System.out.println("У " + mom1);
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Person mom2 = new PersonBuilder()
+                    .setName("Любовь")
+                    .setSurname("Иванова")
+                    .setAge(-10)
+                    .setAddress("Москва")
+                    .build();
+            System.out.println("У " + mom2);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/PersonBuilder.java
@@ -0,0 +1,56 @@
+package org.example;
+import java.util.Objects;
+
+public class PersonBuilder {
+    protected String name;
+    protected String surname;
+    protected String address;
+    protected int age;
+    String t = (String) null;
+
+    public PersonBuilder setName(String name) {
+        if (this.name == null) {
+            this.name = name;
+            return this;
+        }
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        if (this.surname == null) {
+            this.surname = surname;
+            return this;
+        }
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age < 0 || age > 100) {
+            throw new IllegalArgumentException("Некорректный возраст");
+        }
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        Person person = new Person(this.name, this.surname, this.age, this.address);
+        if (surname != null && name != null) {
+            if (person.hasAge()) {
+                if (age > 0 && age < 100) {
+                    return new Person(name, surname, age, address);
+                } else if (age == 0) {
+                    return new Person(name, surname);
+                }
+            }
+        } else {
+            System.out.println(new IllegalStateException("Не задано одно из обязательных полей: Имя и/или фамилия"));
+            return new Person(name, surname);
+        }
+        return new Person(name, surname);
+    }
+}
